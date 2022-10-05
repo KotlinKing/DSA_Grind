64be68ee1d03b3359f5cd2a3c0501e0777d489a8
Abhishek Dharmik <37942793+KotlinKing@users.noreply.github.com>
2022-10-05 23:27:05 +0530

    Update PrimeNumbers.java

--- Mathematics/PrimeNumbers.java
+++ Mathematics/PrimeNumbers.java
@@ -1,4 +1,16 @@
+// Code: Program to find a prime number
+// Name: Abhishek Dharmik
+// Last Modified on: 05/10/2022
+
+// Explanation: we are given a number A and we 
+// need to find out the whether it is 
+// only divisible by 1 and itself.
+
+// Example: a = 13 ; Number is a prime number? true 
+// a = 12 ; Number is a prime number? false
+
 public class PrimeNumbers {
+
     public static void main(String[] args) {
         boolean bool =  isPrime(3);
         System.out.println("Number is a prime number? " + bool);
